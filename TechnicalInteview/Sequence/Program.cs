c0ec1191eed9c6c37b751357f59c06388731889b
kjkasi <5842602+kjkasi@users.noreply.github.com>
2023-01-26 21:25:23 +0300

    Add impl sequence Fibonacci

---
+++ TechnicalInteview/Sequence/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+/*
+0, 1, ...
+Fn = Fn-1 + Fn-2
+
+n F
+1  0 //ошибка, не правильно задал вопрос
+2  1
+3  1
+4  2
+....
+*/
+
+namespace Sequence
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(fn(4));
+        }
+
+        static public int fn(int n)
+        {
+            if (n <= 1) return 0;
+            if (n == 2) return 1;
+
+            int sum = fn(n - 1)  + fn(n - 2);
+
+            return sum;
+        }
+    }
+}
